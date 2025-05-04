421edb8c51a941b0bbb2cb30f530215ce2d81def
bonii880 <b32452834@gmail.com>
2025-05-04 07:57:43 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
